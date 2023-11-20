dc3c8ee4f3ece2a1ce8b49eb41ac0cef211cdab5
Cazyrah <48308492+Cazyrah@users.noreply.github.com>
2023-11-20 19:42:49 +0800

    Update encoder.c

--- hardware_encoder/encoder.c
+++ hardware_encoder/encoder.c
@@ -4,18 +4,12 @@
 #include "hardware/encoder.h"
 
 volatile uint32_t leftNotchCount = 0;
-volatile uint32_t tempLeftNotchCount = 0;
 volatile double leftTotalDistance = 0.0;
-volatile double tempLeftTotalDistance = 0.0;
 volatile uint64_t leftLastNotchTime = 0;
-volatile double leftEncoderSpeed;
 
 volatile uint32_t rightNotchCount = 0;
-volatile uint32_t tempRightNotchCount = 0;
 volatile double rightTotalDistance = 0.0;
-volatile double tempRightTotalDistance = 0.0;
 volatile uint64_t rightLastNotchTime = 0;
-volatile double rightEncoderSpeed;
 
 void gpio_callback_encoder(uint gpio, uint32_t events)
 {
@@ -28,15 +22,13 @@ void gpio_callback_encoder(uint gpio, uint32_t events)
 
             // Increment the count of notches detected for the left wheel
             leftNotchCount++;
-            tempLeftNotchCount++;
 
             // Calculate the total distance traveled by the left wheel in centimeters
             leftTotalDistance = (double)leftNotchCount * CM_PER_NOTCH;
-            tempLeftTotalDistance = (double)tempLeftNotchCount * CM_PER_NOTCH;
-            // printf("LEFT Total Distance: %.2f cm\n", leftTotalDistance);
+            printf("LEFT Total Distance: %.2f cm\n", leftTotalDistance);
 
             // Calculate speed traveled after every cycle
-            if (tempLeftNotchCount % NOTCHES_PER_CYCLE == 0)
+            if (leftNotchCount % NOTCHES_PER_CYCLE == 0)
             {
                 // Get the current time in microseconds
                 uint64_t currentTime = time_us_64();
@@ -47,14 +39,12 @@ void gpio_callback_encoder(uint gpio, uint32_t events)
                 if (timeDiff > 0)
                 {
                     // Calculate and print speed (distance traveled in 1 second) for the left wheel
-                    leftEncoderSpeed = tempLeftTotalDistance * 1e6 / timeDiff;
-                    // printf("LEFT Current Speed: %.2f cm/s\n", leftEncoderSpeed);
+                    double speed_cm_per_s = leftTotalDistance * 1e6 / timeDiff;
+                    printf("LEFT Current Speed: %.2f cm/s\n", speed_cm_per_s);
                 }
 
                 // Update the last notch time for the left wheel
                 leftLastNotchTime = currentTime;
-
-                tempLeftNotchCount = 0;
             }
         }
 
@@ -64,17 +54,14 @@ void gpio_callback_encoder(uint gpio, uint32_t events)
 
             // Increment the count of notches detected for the right wheel
             rightNotchCount++;
-            tempRightNotchCount++;
 
             // Calculate the total distance traveled by the right wheel in centimeters
             rightTotalDistance = (double)rightNotchCount * CM_PER_NOTCH;
-            tempRightTotalDistance = (double)tempRightNotchCount * CM_PER_NOTCH;
-            // printf("RIGHT Total Distance: %.2f cm\n", rightTotalDistance);
+            printf("RIGHT Total Distance: %.2f cm\n", rightTotalDistance);
 
             // Calculate speed traveled after every cycle for the right wheel
-            if (tempRightNotchCount % NOTCHES_PER_CYCLE == 0)
+            if (rightNotchCount % NOTCHES_PER_CYCLE == 0)
             {
-
                 // Get the current time in microseconds
                 uint64_t currentTime = time_us_64();
 
@@ -84,14 +71,12 @@ void gpio_callback_encoder(uint gpio, uint32_t events)
                 if (timeDiff > 0)
                 {
                     // Calculate and print speed (distance traveled in 1 second) for the right wheel
-                    rightEncoderSpeed = tempRightTotalDistance * 1e6 / timeDiff;
-                    // printf("RIGHT Current Speed: %.2f cm/s\n", rightEncoderSpeed);
+                    double speed_cm_per_s = rightTotalDistance * 1e6 / timeDiff;
+                    printf("RIGHT Current Speed: %.2f cm/s\n", speed_cm_per_s);
                 }
 
                 // Update the last notch time for the right wheel
                 rightLastNotchTime = currentTime;
-
-                tempRightNotchCount = 0;
             }
         }
     }
